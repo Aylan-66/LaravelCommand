d179407c15b77ceeace81ef83bb6435b28054c79
Aylan-66 <75484447+Aylan-66@users.noreply.github.com>
2021-08-23 13:05:36 +0200

    Update PostTag.php

--- app/Models/PostTag.php
+++ app/Models/PostTag.php
@@ -8,12 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class PostTag extends Model
 {
     use HasFactory;
-
-    public function scopeGetTagPost($query, $tag_id) {
-
-        $tag = Tag::GetTagPost($tag_id)->get();
-        return $query->where('tag_id', $tag[0]["tag_id"])->select('post_tags.post_id as id');
-      
-    }
-
 }
